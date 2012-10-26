6ac44c9764034c943cbec424a72dd624b03998d2
Shtirlits <tshtirlits@gmail.com>
2012-10-26 15:36:57 +0600

    Сделали запрос к базе через SQL
    Сделали запрос к базе через SQL и мелкую косметику внешнего вида

--- Unit1.pas
+++ Unit1.pas
@@ -4,13 +4,28 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, DB, DBTables, Grids, DBGrids, ExtCtrls, DBCtrls;
+  Dialogs, DB, DBTables, Grids, DBGrids, ExtCtrls, DBCtrls, StdCtrls, Mask,
+  Menus;
 
 type
   TForm1 = class(TForm)
     DBGrid1: TDBGrid;
     DBNavigator1: TDBNavigator;
-    Table1: TTable;
+    Button1: TButton;
+    Query1: TQuery;
+    Button2: TButton;
+    DataSource2: TDataSource;
+    MaskEdit1: TMaskEdit;
+    MaskEdit2: TMaskEdit;
+    ComboBox1: TComboBox;
+    MainMenu1: TMainMenu;
+    A1: TMenuItem;
+    Help1: TMenuItem;
+    About1: TMenuItem;
+    exit1: TMenuItem;
+    procedure Button1Click(Sender: TObject);
+    procedure exit1Click(Sender: TObject);
+  
   private
     { Private declarations }
   public
@@ -23,5 +38,17 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+Form1.Close;
+end;
+
+
+
+procedure TForm1.exit1Click(Sender: TObject);
+begin
+Form1.close;
+end;
 
 end.
